1af51ac94f45f706f010d6108b86e509f1fb8906
George Alexiou <george@alexiou.xyz>
2022-08-31 14:56:55 +0100

    Added pics on the pokedex

--- src/components/Pokedex/PokemonPage.tsx
+++ src/components/Pokedex/PokemonPage.tsx
@@ -1,18 +1,51 @@
 import { StyleSheet, Text, View, FlatList, SafeAreaView, Dimensions, Button } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Image} from 'react-native'
 
 export default function PokemonPage({route, navigation} : any) {
     const {name, url} = route.params;
+    const [sprite, setSprite] = useState<string>()
+    const [colour, setColour] = useState('red');
+    useEffect(() => {
+        console.log('RERENDER')
+        const getPokemonInfo = async () => {
+            try{
+                console.log('started')
+                const response = await fetch(url, {});
+                const json = await response.json();
+                setSprite(json.sprites.front_default)
+            } catch(error){
+                console.log('Error: ', error)
+            };
+        }
+        getPokemonInfo();
+
+    }, [colour]);
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>{name}</Text>
+            <Image
+                style={styles.tinyLogo}
+                source={{
+                uri: sprite,
+                }}
+            />
+            <Button onPress={() => setColour('blue')} title={colour}/>
         </View>
     );
 }
+
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    tinyLogo: {
+        width: 200,
+        height: 200,
     },
 });
