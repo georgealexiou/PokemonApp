6b03dc5ddf9aecf89f3531466a5769f85ea3edfc
George Alexiou <george@alexiou.xyz>
2022-09-02 12:07:15 +0100

    Added Stats and stat bars

--- src/components/Pokedex/PokemonPage.tsx
+++ src/components/Pokedex/PokemonPage.tsx
@@ -9,6 +9,8 @@ import {
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Image } from "react-native";
+import { typeColors } from "../Resource/Resource";
+import { setStatusBarBackgroundColor } from "expo-status-bar";
 
 /*
 TODO: Stuff I want to show here:
@@ -17,14 +19,33 @@ TODO: Stuff I want to show here:
  * Description
 */
 
+const { width, height } = Dimensions.get("window");
+
+type RenderItemProps = {
+  item: {
+    effort: number;
+    base_stat: number;
+    stat: {
+      name: string;
+      url: string;
+    };
+  };
+};
+
 const capitalizeFirstLetter = (str: string) => {
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  if (str != undefined)
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  else return "";
 };
 
 export default function PokemonPage({ route, navigation }: any) {
   var { name, url } = route.params;
   name = capitalizeFirstLetter(name);
   const [sprite, setSprite] = useState<string>();
+  const [types, setType] = useState<string[]>();
+  const [ability, setAbility] = useState<string>();
+  const [id, setId] = useState<string>();
+  const [stats, setStats] = useState<RenderItemProps[]>();
 
   //fetch pokemon details from api
   useEffect(() => {
@@ -32,34 +53,132 @@ export default function PokemonPage({ route, navigation }: any) {
       try {
         const response = await fetch(url, {});
         const json = await response.json();
+
         setSprite(json.sprites.front_default);
+        const types = [];
+        types[0] = capitalizeFirstLetter(json.types[0].type.name);
+        types[1] = capitalizeFirstLetter(json.types[1]?.type.name);
+        setType(types);
+        setAbility(capitalizeFirstLetter(json.abilities[0].ability.name));
+        setId(json.game_indices[0].game_index);
+        setStats(json.stats);
+        console.log(stats);
       } catch (error) {
-        console.log("Error: ", error);
+        //console.log("Error: ", error);
       }
     };
     getPokemonInfo();
-  });
+  }, []);
+
+  //* Function to render Stats
+  const renderStats: React.FC<RenderItemProps> = ({ item }) => {
+    return (
+      <View>
+        <Text>ff</Text>
+      </View>
+    );
+  };
+
+  if (types === undefined || stats === undefined) {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
 
   return (
-    <View style={{ alignItems: "center", margin: 20 }}>
-      <Text style={styles.titleText}>{name}</Text>
-      <View style={{ flexDirection: "row" }}>
-        <View>
-          <Image
-            style={styles.tinyLogo}
-            source={{
-              uri: sprite,
-            }}
-          />
-          <Text>Sprite</Text>
+    <View style={{ flex: 1, backgroundColor: typeColors.get(types[0]) }}>
+      <View style={{ margin: 20 }}>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.titleText}>{name}</Text>
         </View>
-        <View style={{ marginTop: 20 }}>
-          <Text>Table</Text>
+
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ alignItems: "stretch" }}>
+            <Image
+              style={styles.tinyLogo}
+              source={{
+                uri: sprite,
+              }}
+            />
+          </View>
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: "row",
+            }}
+          >
+            <View style={{ marginRight: 10 }}>
+              <Text style={{ fontWeight: "bold" }}>Pokedex Entry</Text>
+              <Text style={{ fontWeight: "bold" }}>Type </Text>
+              <Text style={{ fontWeight: "bold" }}>Ability </Text>
+            </View>
+            <View>
+              <Text>{id}</Text>
+              <Text>{ability}</Text>
+              <Text>{types}</Text>
+            </View>
+          </View>
+        </View>
+
+        {/* stats */}
+        <View>
+          <Text style={{ fontWeight: "bold", fontSize: 20 }}>Stats</Text>
+          <View style={{ flexDirection: "row" }}>
+            <View style={{ marginRight: 10 }}>
+              <Text style={{ marginTop: 5.5, fontWeight: "bold" }}>HP</Text>
+              <Text style={{ marginTop: 5.5, fontWeight: "bold" }}>Attack</Text>
+              <Text style={{ marginTop: 5.5, fontWeight: "bold" }}>
+                Defence
+              </Text>
+              <Text style={{ marginTop: 5.5, fontWeight: "bold" }}>
+                Special Attack
+              </Text>
+              <Text style={{ marginTop: 5.5, fontWeight: "bold" }}>
+                Special Defence
+              </Text>
+              <Text style={{ marginTop: 5.5, fontWeight: "bold" }}>Speed</Text>
+            </View>
+            <View>
+              <StatBar stat={stats[0].base_stat}></StatBar>
+              <StatBar stat={stats[1].base_stat}></StatBar>
+              <StatBar stat={stats[2].base_stat}></StatBar>
+              <StatBar stat={stats[3].base_stat}></StatBar>
+              <StatBar stat={stats[4].base_stat}></StatBar>
+              <StatBar stat={stats[5].base_stat}></StatBar>
+            </View>
+          </View>
+          {/* <SafeAreaView style={styles.separateStats}>
+            <FlatList>
+              <FlatList data={stats} renderItem={renderStats} />
+            </FlatList>
+          </SafeAreaView> */}
         </View>
       </View>
     </View>
   );
 }
+
+type StatBarProps = {
+  stat: number;
+};
+
+const StatBar: React.FC<StatBarProps> = ({ stat }) => {
+  const percent = stat / 255;
+  return (
+    <View
+      style={{
+        marginTop: 5,
+        width: (width / 2) * percent,
+        height: 16,
+        backgroundColor: "black",
+      }}
+    >
+      <Text style={{ color: "white" }}>{stat}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -69,11 +188,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   tinyLogo: {
-    width: 200,
-    height: 200,
+    width: width / 2.5,
+    height: height / 5,
   },
   titleText: {
     fontSize: 30,
     fontWeight: "bold",
+  },
+  separateStats: {
+    width,
+    fontSize: 20,
   },
 });
--- src/components/Resource/Resource.tsx
+++ src/components/Resource/Resource.tsx
@@ -8,7 +8,28 @@ import alolaList from "../../../assets/regions/alola.json";
 import galarList from "../../../assets/regions/galar.json";
 import paldeaList from "../../../assets/regions/paldea.json";
 
-export function getRegionalDex(id) {
+export const typeColors = new Map([
+  ["Normal", "#A8A77A"],
+  ["Fire", "#EE8130"],
+  ["Water", "#6390F0"],
+  ["Electric", "#F7D02C"],
+  ["Grass", "#7AC74C"],
+  ["Ice", "#96D9D6"],
+  ["Fighting", "#C22E28"],
+  ["Poison", "#A33EA1"],
+  ["Ground", "#E2BF65"],
+  ["Flying", "#A98FF3"],
+  ["Psychic", "#F95587"],
+  ["Bug", "#A6B91A"],
+  ["Rock", "#B6A136"],
+  ["Ghost", "#735797"],
+  ["Dragon", "#6F35FC"],
+  ["Dark", "#705746"],
+  ["Steel", "#B7B7CE"],
+  ["Fairy", "#D685AD"],
+]);
+
+export function getRegionalDex(id: number) {
   switch (id) {
     case 1:
       return kantoList;
