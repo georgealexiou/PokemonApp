8119c5217c402f2a1e27c09b5e40e4426e76b58e
George Alexiou <george@alexiou.xyz>
2023-02-08 14:23:06 +0000

    Updated stat bar

--- src/components/Atoms/StatBar/StatBar.tsx
+++ src/components/Atoms/StatBar/StatBar.tsx
@@ -1,5 +1,6 @@
-import { Dimensions, View, Text } from "react-native";
-import { styles } from "./styles";
+import { Dimensions, View, Text } from 'react-native';
+import { textStyle } from '../../Resource/textStyle';
+import { styles } from './styles';
 
 type StatBarProps = {
   statName: string;
@@ -10,20 +11,14 @@ type StatBarProps = {
 export const StatBar: React.FC<StatBarProps> = ({ statName, stat, color }) => {
   const percent = stat / 255;
   const width = 340;
-  // var color = "#F34444";
-  // if (stat > 30) color = "#F87E10";
-  // if (stat > 60) color = "#FDDD57";
-  // if (stat > 100) color = "#A0E514";
-  // if (stat > 130) color = "#43CD5F";
-  // if (stat > 200) color = "#3CC2B8";
 
   return (
-    <View style={{ flexDirection: "row", width: "100%", marginTop: 4 }}>
-      <View style={{ width: "42%" }}>
-        <Text style={styles.statsTextLabel}>{statName}</Text>
+    <View style={{ flexDirection: 'row', width: '100%', marginTop: 4 }}>
+      <View style={{ width: '42%' }}>
+        <Text style={textStyle.h2}>{statName}</Text>
       </View>
-      <View style={{ width: "11%" }}>
-        <Text style={styles.statsTextNumber}>{stat}</Text>
+      <View style={{ width: '11%' }}>
+        <Text style={textStyle.caption}>{stat}</Text>
       </View>
       <View
         style={{
@@ -33,7 +28,7 @@ export const StatBar: React.FC<StatBarProps> = ({ statName, stat, color }) => {
           backgroundColor: color,
           marginTop: 6,
         }}
-      ></View>
+      />
     </View>
   );
 };
