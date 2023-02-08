85941568592351609edb17777752c451e14d661e
George Alexiou <george@alexiou.xyz>
2023-02-08 14:22:45 +0000

    Updated generations design

--- src/Features/GenerationsScreen/GenerationsScreen.tsx
+++ src/Features/GenerationsScreen/GenerationsScreen.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Image, Text, View, FlatList, SafeAreaView, ImageBackground, Pressable } from 'react-native';
 import { Pokeball } from '../../assets/svgs';
 import Screen from '../../Components/Screen/Screen';
-import { regions } from '../../global/generations';
+import regions from '../../assets/regions/regions.json';
 import { styles } from './styles';
+import { Generation } from '../../global/types';
+import { SimplePressable } from '../../Components/Atoms/SimplePressable/SimplePressable';
+import { Spacer } from '../../Components/Atoms/Spacer.tsx/Spacer';
+import { textStyle } from '../../Components/Resource/textStyle';
+import { PokemonImage } from '../../Components/Atoms/PokemonImage/PokemonImage';
 
 type RenderItemProps = {
-  item: {
-    id: number;
-    name: string;
-    url: string;
-  };
+  item: Generation;
 };
 
 type GenerationScreenProps = {
@@ -18,21 +19,84 @@ type GenerationScreenProps = {
 };
 
 export const GenerationsScreen: React.FC<GenerationScreenProps> = ({ navigation }) => {
-  const renderItem = ({ item }: any) => {
+  const renderItem: React.FC<RenderItemProps> = ({ item }) => {
     const onPress = (arg: RenderItemProps) => () => {
-      navigation.navigate('PokemonListScreen', item);
+      const generateList = () => {
+        const list = [];
+        for (let i = item.firstPokemonId; i <= item.lastPokemonId; i++) {
+          list.push(i);
+        }
+        return list;
+      };
+      navigation.navigate('PokemonListScreen', generateList());
     };
 
+    const starterPokemon = [];
+    if (item.id === 5) {
+      starterPokemon.push(item.firstPokemonId + 1);
+      starterPokemon.push(item.firstPokemonId + 4);
+      starterPokemon.push(item.firstPokemonId + 7);
+    } else {
+      starterPokemon.push(item.firstPokemonId);
+      starterPokemon.push(item.firstPokemonId + 3);
+      starterPokemon.push(item.firstPokemonId + 6);
+    }
+
     return (
-      <Pressable style={styles.generationContainer} onPress={onPress({ item })}>
-        <Text>{item.name}</Text>
-      </Pressable>
+      <View style={{ paddingHorizontal: 20, paddingVertical: 5 }}>
+        <SimplePressable
+          onPress={onPress({ item })}
+          style={{
+            width: '100%',
+            height: 100,
+          }}>
+          <View style={{ padding: 10, flexDirection: 'row', flex: 1 }}>
+            <View>
+              <Text style={textStyle.caption}>{`Generation ${item.id}`}</Text>
+              <Text style={textStyle.h1}>{item.region}</Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                flex: 1,
+                height: '100%',
+                justifyContent: 'flex-end',
+                alignItems: 'flex-end',
+              }}>
+              <PokemonImage
+                id={starterPokemon[0]}
+                style={{
+                  width: 70,
+                  height: 70,
+                  resizeMode: 'contain',
+                }}
+              />
+              <PokemonImage
+                id={starterPokemon[1]}
+                style={{
+                  width: 70,
+                  height: 70,
+                  resizeMode: 'contain',
+                }}
+              />
+              <PokemonImage
+                id={starterPokemon[2]}
+                style={{
+                  width: 70,
+                  height: 70,
+                  resizeMode: 'contain',
+                }}
+              />
+            </View>
+          </View>
+        </SimplePressable>
+      </View>
     );
   };
 
   return (
     <Screen name={'Generations'} safeAreaRequired={false}>
-      <View style={{ width: '100%' }}>
+      <View style={{ width: '100%', height: '100%' }}>
         <FlatList data={regions} renderItem={renderItem} />
       </View>
       <View style={{ position: 'absolute', zIndex: -1, height: '100%', justifyContent: 'flex-start', right: -100 }}>
--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import React, { FunctionComponent, useMemo } from "react";
-import { View, Text, Pressable } from "react-native";
-import { SafeAreaView, Edge } from "react-native-safe-area-context";
-import { theme } from "../../../themes/theme";
-import { styles } from "./style";
-import Icon from "react-native-vector-icons/FontAwesome";
+import { StatusBar } from 'expo-status-bar';
+import React, { FunctionComponent, useMemo } from 'react';
+import { View, Text, Pressable } from 'react-native';
+import { SafeAreaView, Edge } from 'react-native-safe-area-context';
+import { theme } from '../../../themes/theme';
+import { styles } from './style';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export type ScreenAreaProps = {
   name: string;
@@ -23,73 +23,65 @@ export const Screen: FunctionComponent<ScreenAreaProps> = ({
   onBackButtonPress,
   children,
   safeAreaRequired,
-  bottomSafeArea,
+  bottomSafeArea = false,
   topSafeArea,
-  backgroundColor = "white",
+  backgroundColor = 'white',
   whiteText,
 }) => {
   const getSafeAreaViewEdges = (): Edge[] => {
     if (safeAreaRequired) {
-      return ["top", "bottom", "left", "right"];
+      return ['top', 'bottom', 'left', 'right'];
     } else if (bottomSafeArea) {
-      return ["bottom", "left", "right"];
+      return ['bottom', 'left', 'right'];
     } else if (topSafeArea) {
-      return ["top", "left", "right"];
+      return ['top', 'left', 'right'];
     }
-    return ["left", "right"];
+    return ['left', 'right'];
   };
 
   return (
     <SafeAreaView
       style={{ ...styles.styledWhiteView, backgroundColor }}
       testID="safeAreaView"
-      edges={getSafeAreaViewEdges()}
-    >
+      edges={getSafeAreaViewEdges()}>
       {whiteText ? (
         <Text
           style={{
             fontSize: 30,
-            fontWeight: "bold",
-            color: "white",
-            alignSelf: "center",
-            position: "absolute",
+            fontWeight: 'bold',
+            color: 'white',
+            alignSelf: 'center',
+            position: 'absolute',
             top: 50,
-          }}
-        >
+          }}>
           {name}
         </Text>
       ) : (
         <Text
           style={{
             fontSize: 30,
-            alignSelf: "center",
-            position: "absolute",
+            alignSelf: 'center',
+            position: 'absolute',
             top: 50,
-          }}
-        >
+          }}>
           {name}
         </Text>
       )}
       <View
         style={{
-          width: "100%",
+          width: '100%',
           height: 90,
-          justifyContent: "flex-end",
-          flexDirection: "row",
-        }}
-      >
-        <Pressable
-          style={{ width: "37%", justifyContent: "flex-end", top: -5 }}
-          onPress={onBackButtonPress}
-        >
+          justifyContent: 'flex-end',
+          flexDirection: 'row',
+        }}>
+        <Pressable style={{ width: '37%', justifyContent: 'flex-end', top: -5 }} onPress={onBackButtonPress}>
           <Icon name="chevron-left" color="black" size={25} />
         </Pressable>
         <View
           style={{
-            width: "60%",
-            justifyContent: "flex-end",
-          }}
-        ></View>
+            width: '60%',
+            justifyContent: 'flex-end',
+          }}></View>
       </View>
       <View style={{ ...styles.styledWhiteView }}>{children}</View>
     </SafeAreaView>
