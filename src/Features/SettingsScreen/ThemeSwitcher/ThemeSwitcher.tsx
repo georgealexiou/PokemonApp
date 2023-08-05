f9bbc5fccf0a85ad7ee277d922897b00979f45af
Kofi Twum-Ampofo <pktwum96@gmail.com>
2023-08-05 12:44:22 +0100

    update theme swicther style

--- src/Features/SettingsScreen/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/Features/SettingsScreen/ThemeSwitcher/ThemeSwitcher.tsx
@@ -7,23 +7,53 @@ import { lightTheme } from '../../../../themes/light-theme';
 import { darkTheme } from '../../../../themes/dark-theme';
 import { ThemeContext } from '../../../../themes/theme-context';
 
+import { ButtonGroup } from 'react-native-elements';
+
 export const ThemeSwitcher = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => {
-    setIsEnabled((previousState) => !previousState);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const updateIndex = (index: number) => {
+    setSelectedIndex(index);
   };
   const setTheme = useContext(ThemeContext).setTheme;
 
   useEffect(() => {
-    setTheme(isEnabled ? darkTheme : lightTheme);
-  }, [isEnabled]);
+    let theme = lightTheme;
+    switch (selectedIndex) {
+      case 0:
+        break;
+      case 1:
+        theme = darkTheme;
+        break;
+      case 2:
+        theme = lightTheme;
+    }
+    setTheme(theme);
+  }, [selectedIndex]);
 
   const theme = useTheme();
   const styles = useStyleSheet(theme);
+  const buttons = ['Automatic', 'Dark Mode', 'Light Mode'];
+
   return (
     <SafeAreaView style={styles.container}>
-      <Switch onValueChange={toggleSwitch} value={isEnabled} />
-      <Text style={styles.text}>Dark Mode</Text>
+      <Text style={styles.text}>Theme</Text>
+      <ButtonGroup
+        onPress={updateIndex}
+        selectedIndex={selectedIndex}
+        buttons={buttons}
+        buttonStyle={{
+          backgroundColor: theme.primaryBackgroundColor,
+        }}
+        textStyle={{
+          color: theme.primaryTextColor,
+        }}
+        selectedButtonStyle={{
+          backgroundColor: theme.tertiaryBackgroundColor,
+        }}
+        containerStyle={{ height: 100, borderRadius: 10 }}
+        vertical
+      />
     </SafeAreaView>
   );
 };
@@ -31,7 +61,7 @@ export const ThemeSwitcher = () => {
 const useStyleSheet = (theme: Theme) =>
   StyleSheet.create({
     container: {
-      flexDirection: 'row',
+      padding: 6,
     },
-    text: { color: theme.primaryTextColor },
+    text: { color: theme.primaryTextColor, marginVertical: 5, marginHorizontal: 10 },
   });
